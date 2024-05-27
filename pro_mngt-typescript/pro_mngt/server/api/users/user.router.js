aa90d41d6cddd191c9772b981218abc726e432fb
HeenaTgs <heena.prajapati@triveniglobalsoft.com>
2024-05-27 10:06:18 +0530

    Modified files

--- pro_mngt-typescript/pro_mngt/server/api/users/user.router.js
+++
@@ -1,9 +0,0 @@
-const user = require("../users/user.service");
-  
-var router = require("express").Router();
-
-// Create a new user
-router.post("/", user.create);
-router.post("/login", user.login);
-
-module.exports = router;
